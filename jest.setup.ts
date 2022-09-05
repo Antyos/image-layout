adf3b4acde4482390d2e006db802a2c9eb7e965c
Andrew Glick <17516195+Antyos@users.noreply.github.com>
2022-09-05 15:31:52 -0500

    test: Throw error without Jest's global expect

--- jest.setup.ts
+++ jest.setup.ts
@@ -6,8 +6,15 @@ interface MaybeHasJestExpect {
     expect?: jest.Expect;
 }
 
+// Based on: https://github.com/jest-community/jest-extended/blob/2bd088758d08a5ada82f28d26757f1d7f4ccaca4/src/all/index.js
 const jestExpect = (global as unknown as MaybeHasJestExpect).expect;
 
-if (jestExpect !== undefined) {
-    jestExpect.extend({ toMatchCloseTo, toBeDeepCloseTo });
+if (jestExpect === undefined) {
+    throw new Error(
+        "Unable to find Jest's global expect. " +
+            'Please check you have added jest-extended correctly to your jest configuration. ' +
+            'See https://github.com/jest-community/jest-extended#setup for help.',
+    );
 }
+
+jestExpect.extend({ toMatchCloseTo, toBeDeepCloseTo });
