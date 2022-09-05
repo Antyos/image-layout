762422375ed415628f26c2b1c94e4bd8ef603284
Andrew Glick <17516195+Antyos@users.noreply.github.com>
2022-09-05 15:19:25 -0500

    test: Patch jest-matcher-deep-close-to into expect()

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,3 +1,13 @@
 import { toMatchCloseTo, toBeDeepCloseTo } from 'jest-matcher-deep-close-to';
 
-expect.extend({ toMatchCloseTo, toBeDeepCloseTo });
+// Patch `toMatchCloseTo` and `toBeDeepCloseTo` into jest's expect()
+// I'm not really if this is a good idea, but it seems to work
+interface MaybeHasJestExpect {
+    expect?: jest.Expect;
+}
+
+const jestExpect = (global as unknown as MaybeHasJestExpect).expect;
+
+if (jestExpect !== undefined) {
+    jestExpect.extend({ toMatchCloseTo, toBeDeepCloseTo });
+}
