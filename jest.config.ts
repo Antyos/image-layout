aa1224fea043d66f05c0fbbeb3ff09ab9c6fe2f5
Andrew Glick <17516195+Antyos@users.noreply.github.com>
2022-09-05 14:31:53 -0500

    test: Jest setup with jest-matcher-deep-close-to

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
-    rootDir: 'src',
     preset: 'ts-jest/presets/js-with-ts-esm',
     testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['./jest.setup.ts'],
 };
--- src/image-layout.test.ts
+++ src/image-layout.test.ts
@@ -1,5 +1,6 @@
 import {layoutGridByRows} from './image-layout';
 
+
 describe("Test Grid Layout", () => {
     test("Several rows, full width, no spacing", () => {
         const aspects = [
@@ -8,12 +9,12 @@ describe("Test Grid Layout", () => {
         ]
 
         const layout = layoutGridByRows(aspects, {maxWidth: 300, spacing: 0});
-        expect(layout.positions).toStrictEqual([
+        expect(layout.positions).toBeDeepCloseTo([
             {height: 80, width: 100, x: 0, y: 0},
             {height: 80, width: 120, x: 100, y: 0},
             {height: 80, width: 80, x: 220, y: 0},
             {height: 120, width: 90, x: 0, y: 80},
             {height: 120, width: 210, x: 90, y: 80},
-        ]);
+        ], 3);
     });
 });
